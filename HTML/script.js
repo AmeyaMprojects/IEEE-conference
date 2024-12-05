283609b4a5154a253f20889fc5385bb931ba3720
Ameya Mhatre <ameyam.projects@gmail.com>
2024-12-05 16:58:39 +0530

    minor updates

--- HTML/script.js
+++
@@ -1,6 +0,0 @@
-const aboutButton = document.querySelector('.nav-links a[href="#About"]'); // Select the "About" button
-const aboutConferenceSection = document.querySelector('.about-conference'); // Select the target section
-
-aboutButton.addEventListener('click', () => {
-  aboutConferenceSection.scrollIntoView({ behavior: 'smooth' }); // Scroll smoothly to the section
-});
